6db38c07933a66502ad9e39fcedc2ae8fe17388b
bugaved <bugaved@gmail.com>
2015-11-05 15:20:04 +0300

    commit9

--- HomeWork55/src/horse/HomeWork55.java
+++ HomeWork55/src/horse/HomeWork55.java
@@ -3,135 +3,136 @@ package horse;
 import horse.Num;
 import static horse.Num.m;
 import static horse.Num.i;
-import static horse.Num.k;
+import static horse.Num.j;
 
 public class HomeWork55 {
 
     public static void main(String[] args) {
         int ni = 0;
-        int nk = 0;
+        int nj = 0;
 
-        for (int x = 0; x < i * k; x++) {
+        for (int x = 0; x < i * j; x++) {
             int k = 12;
-            m[ni][nk] = 1;
+            m[ni][nj] = 1;
             boolean turn = false;
-            Num xp2p1 = new Num(ni + 2, nk + 1);
-            Num xp2m1 = new Num(ni + 2, nk - 1);
-            Num xm2p1 = new Num(ni - 2, nk + 1);
-            Num xm2m1 = new Num(ni - 2, nk - 1);
-            Num xp1p2 = new Num(ni + 1, nk + 2);
-            Num xp1m2 = new Num(ni + 1, nk - 2);
-            Num xm1p2 = new Num(ni - 1, nk + 2);
-            Num xm1m2 = new Num(ni - 1, nk - 2);
-            if ((ni + 2 >= 0) && (nk + 1 >= 0) && (ni + 2 < i) && (nk + 1 < k)) {
-                if ((xp2p1.getNumOfHits() < k) && (m[ni + 2][nk + 1] == 0)) {
+            Num xp2p1 = new Num(ni + 2, nj + 1);
+            Num xp2m1 = new Num(ni + 2, nj - 1);
+            Num xm2p1 = new Num(ni - 2, nj + 1);
+            Num xm2m1 = new Num(ni - 2, nj - 1);
+            Num xp1p2 = new Num(ni + 1, nj + 2);
+            Num xp1m2 = new Num(ni + 1, nj - 2);
+            Num xm1p2 = new Num(ni - 1, nj + 2);
+            Num xm1m2 = new Num(ni - 1, nj - 2);
+            if ((ni + 2 >= 0) && (nj + 1 >= 0) && (ni + 2 < i) && (nj + 1 < j)) {
+                if ((xp2p1.getNumOfHits() < k) && (m[ni + 2][nj + 1] == 0)) {
                     k = xp2p1.getNumOfHits();
                 }
             }
-            if ((ni + 2 >= 0) && (nk - 1 >= 0) && (ni + 2 < i) && (nk - 1 < k)) {
-                if ((xp2m1.getNumOfHits() < k) && (m[ni + 2][nk - 1] == 0)) {
+            if ((ni + 2 >= 0) && (nj - 1 >= 0) && (ni + 2 < i) && (nj - 1 < j)) {
+                if ((xp2m1.getNumOfHits() < k) && (m[ni + 2][nj - 1] == 0)) {
                     k = xp2m1.getNumOfHits();
                 }
             }
-            if ((ni - 1 >= 0) && (nk - 2 >= 0) && (ni - 1 < i) && (nk - 2 < k)) {
-                if ((xm1m2.getNumOfHits() < k) && (m[ni - 1][nk - 2] == 0)) {
+            if ((ni - 1 >= 0) && (nj - 2 >= 0) && (ni - 1 < i) && (nj - 2 < j)) {
+                if ((xm1m2.getNumOfHits() < k) && (m[ni - 1][nj - 2] == 0)) {
                     k = xm1m2.getNumOfHits();
                 }
             }
-            if ((ni - 2 >= 0) && (nk + 1 >= 0) && (ni - 2 < i) && (nk + 1 < k)) {
-                if ((xm2p1.getNumOfHits() < k) && (m[ni - 2][nk + 1] == 0)) {
+            if ((ni - 2 >= 0) && (nj + 1 >= 0) && (ni - 2 < i) && (nj + 1 < j)) {
+                if ((xm2p1.getNumOfHits() < k) && (m[ni - 2][nj + 1] == 0)) {
                     k = xm2p1.getNumOfHits();
                 }
             }
-            if ((ni - 2 >= 0) && (nk - 1 >= 0) && (ni - 2 < i) && (nk - 1 < k)) {
-                if ((xm2m1.getNumOfHits() < k) && (m[ni - 2][nk - 1] == 0)) {
+            if ((ni - 2 >= 0) && (nj - 1 >= 0) && (ni - 2 < i) && (nj - 1 < j)) {
+                if ((xm2m1.getNumOfHits() < k) && (m[ni - 2][nj - 1] == 0)) {
                     k = xm2m1.getNumOfHits();
                 }
             }
-            if ((ni + 1 >= 0) && (nk + 2 >= 0) && (ni + 1 < i) && (nk + 2 < k)) {
-                if ((xp1p2.getNumOfHits() < k) && (m[ni + 1][nk + 2] == 0)) {
+            if ((ni + 1 >= 0) && (nj + 2 >= 0) && (ni + 1 < i) && (nj + 2 < j)) {
+                if ((xp1p2.getNumOfHits() < k) && (m[ni + 1][nj + 2] == 0)) {
                     k = xp1p2.getNumOfHits();
+
                 }
             }
-            if ((ni + 1 >= 0) && (nk - 2 >= 0) && (ni + 1 < i) && (nk - 2 < k)) {
-                if ((xp1m2.getNumOfHits() < k) && (m[ni + 1][nk - 2] == 0)) {
+            if ((ni + 1 >= 0) && (nj - 2 >= 0) && (ni + 1 < i) && (nj - 2 < j)) {
+                if ((xp1m2.getNumOfHits() < k) && (m[ni + 1][nj - 2] == 0)) {
                     k = xp1m2.getNumOfHits();
                 }
             }
-            if ((ni - 1 >= 0) && (nk + 2 >= 0) && (ni - 1 < i) && (nk + 2 < k)) {
-                if ((xm1p2.getNumOfHits() < k) && (m[ni - 1][nk + 2] == 0)) {
+            if ((ni - 1 >= 0) && (nj + 2 >= 0) && (ni - 1 < i) && (nj + 2 < j)) {
+                if ((xm1p2.getNumOfHits() < k) && (m[ni - 1][nj + 2] == 0)) {
                     k = xm1p2.getNumOfHits();
                 }
             }
-            if ((ni + 2 >= 0) && (nk + 1 >= 0) && (ni + 2 < i) && (nk + 1 < k)) {
-                if ((xp2p1.getNumOfHits() == k) && (turn == false) && (m[ni + 2][nk + 1] == 0)) {
+            if ((ni + 2 >= 0) && (nj + 1 >= 0) && (ni + 2 < i) && (nj + 1 < j)) {
+                if ((xp2p1.getNumOfHits() == k) && (turn == false) && (m[ni + 2][nj + 1] == 0)) {
 
                     ni = ni + 2;
-                    nk = nk + 1;
-                    System.out.println(ni + " " + nk);
+                    nj = nj + 1;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni + 2 >= 0) && (nk - 1 >= 0) && (ni + 2 < i) && (nk - 1 < k)) {
-                if ((xp2m1.getNumOfHits() == k) && (turn == false) && (m[ni + 2][nk - 1] == 0)) {
+            if ((ni + 2 >= 0) && (nj - 1 >= 0) && (ni + 2 < i) && (nj - 1 < j)) {
+                if ((xp2m1.getNumOfHits() == k) && (turn == false) && (m[ni + 2][nj - 1] == 0)) {
 
                     ni = ni + 2;
-                    nk = nk - 1;
-                    System.out.println(ni + " " + nk);
+                    nj = nj - 1;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni - 2 >= 0) && (nk + 1 >= 0) && (ni - 2 < i) && (nk + 1 < k)) {
-                if ((xm2p1.getNumOfHits() == k) && (turn == false) && (m[ni - 2][nk + 1] == 0)) {
+            if ((ni - 2 >= 0) && (nj + 1 >= 0) && (ni - 2 < i) && (nj + 1 < j)) {
+                if ((xm2p1.getNumOfHits() == k) && (turn == false) && (m[ni - 2][nj + 1] == 0)) {
 
                     ni = ni - 2;
-                    nk = nk + 1;
-                    System.out.println(ni + " " + nk);
+                    nj = nj + 1;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni - 2 >= 0) && (nk - 1 >= 0) && (ni - 2 < i) && (nk - 1 < k)) {
-                if ((xm2m1.getNumOfHits() == k) && (turn == false) && (m[ni - 2][nk - 1] == 0)) {
+            if ((ni - 2 >= 0) && (nj - 1 >= 0) && (ni - 2 < i) && (nj - 1 < j)) {
+                if ((xm2m1.getNumOfHits() == k) && (turn == false) && (m[ni - 2][nj - 1] == 0)) {
 
                     ni = ni - 2;
-                    nk = nk - 1;
-                    System.out.println(ni + " " + nk);
+                    nj = nj - 1;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni + 1 >= 0) && (nk + 2 >= 0) && (ni + 1 < i) && (nk + 2 < k)) {
-                if ((xp1p2.getNumOfHits() == k) && (turn == false) && (m[ni + 1][nk + 2] == 0)) {
+            if ((ni + 1 >= 0) && (nj + 2 >= 0) && (ni + 1 < i) && (nj + 2 < j)) {
+                if ((xp1p2.getNumOfHits() == k) && (turn == false) && (m[ni + 1][nj + 2] == 0)) {
 
                     ni = ni + 1;
-                    nk = nk + 2;
-                    System.out.println(ni + " " + nk);
+                    nj = nj + 2;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni + 1 >= 0) && (nk - 2 >= 0) && (ni + 1 < i) && (nk - 2 < k)) {
-                if ((xp1m2.getNumOfHits() == k) && (turn == false) && (m[ni + 1][nk - 2] == 0)) {
+            if ((ni + 1 >= 0) && (nj - 2 >= 0) && (ni + 1 < i) && (nj - 2 < j)) {
+                if ((xp1m2.getNumOfHits() == k) && (turn == false) && (m[ni + 1][nj - 2] == 0)) {
 
                     ni = ni + 1;
-                    nk = nk - 2;
-                    System.out.println(ni + " " + nk);
+                    nj = nj - 2;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni - 1 >= 0) && (nk + 2 >= 0) && (ni - 1 < i) && (nk + 2 < k)) {
-                if ((xm1p2.getNumOfHits() == k) && (turn == false) && (m[ni - 1][nk + 2] == 0)) {
+            if ((ni - 1 >= 0) && (nj + 2 >= 0) && (ni - 1 < i) && (nj + 2 < j)) {
+                if ((xm1p2.getNumOfHits() == k) && (turn == false) && (m[ni - 1][nj + 2] == 0)) {
 
                     ni = ni - 1;
-                    nk = nk + 2;
-                    System.out.println(ni + " " + nk);
+                    nj = nj + 2;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
-            if ((ni - 1 >= 0) && (nk - 2 >= 0) && (ni - 1 < i) && (nk - 2 < k)) {
-                if ((xm1m2.getNumOfHits() == k) && (turn == false) && (m[ni - 1][nk - 2] == 0)) {
+            if ((ni - 1 >= 0) && (nj - 2 >= 0) && (ni - 1 < i) && (nj - 2 < j)) {
+                if ((xm1m2.getNumOfHits() == k) && (turn == false) && (m[ni - 1][nj - 2] == 0)) {
 
                     ni = ni - 1;
-                    nk = nk - 2;
-                    System.out.println(ni + " " + nk);
+                    nj = nj - 2;
+                    System.out.println(ni + " " + nj);
                     turn = true;
                 }
             }
--- HomeWork55/src/horse/Num.java
+++ HomeWork55/src/horse/Num.java
@@ -4,13 +4,12 @@ public class Num {
 
     private int numOfHits = 0;
     private int ni;
-    private int nk;
-    private int mik;
+    private int nj;
 
     public static final int i = 5;
-    public static final int k = 5;
+    public static final int j = 5;
 
-    public static int m[][] = new int[i][k];
+    public static int m[][] = new int[i][j];
 
     public int getNumOfHits() {
         return numOfHits;
@@ -18,51 +17,52 @@ public class Num {
 
     public Num(int ni, int nk) {
         this.ni = ni;
-        this.nk = nk;
+        this.nj = nk;
         Method1();
 
     }
 
     public void Method1() {
-        if ((ni < i) && (ni >= 0) && (nk < k) && (nk >= 0)) {
-            if ((ni + 2 < i) && (nk + 1 < k) && (ni + 2 >= 0) && (nk + 1 >= 0)) {
-                if ((m[ni + 2][nk + 1] == 0)) {
+        if ((ni < i) && (ni >= 0) && (nj < j) && (nj >= 0)) {
+          
+            if ((ni + 2 < i) && (nj + 1 < j) && (ni + 2 >= 0) && (nj + 1 >= 0)) {
+                if ((m[ni + 2][nj + 1] == 0)) {
                     numOfHits++;
                 }
             }
 
-            if ((ni - 2 < i) && (nk + 1 < k) && (ni - 2 >= 0) && (nk + 1 >= 0)) {
-                if (m[ni - 2][nk + 1] == 0) {
+            if ((ni - 2 < i) && (nj + 1 < j) && (ni - 2 >= 0) && (nj + 1 >= 0)) {
+                if (m[ni - 2][nj + 1] == 0) {
                     numOfHits++;
                 }
             }
-            if ((ni + 2 < i) && (nk - 1 < k) && (ni + 2 >= 0) && (nk - 1 >= 0)) {
-                if ((m[ni + 2][nk - 1] == 0)) {
+            if ((ni + 2 < i) && (nj - 1 < j) && (ni + 2 >= 0) && (nj - 1 >= 0)) {
+                if ((m[ni + 2][nj - 1] == 0)) {
                     numOfHits++;
                 }
             }
-            if ((ni - 2 < i) && (nk - 1 < k) && (ni - 2 >= 0) && (nk - 1 >= 0)) {
-                if ((m[ni - 2][nk - 1] == 0)) {
+            if ((ni - 2 < i) && (nj - 1 < j) && (ni - 2 >= 0) && (nj - 1 >= 0)) {
+                if ((m[ni - 2][nj - 1] == 0)) {
                     numOfHits++;
                 }
             }
-            if ((ni + 1 < i) && (nk + 2 < k) && (ni + 1 >= 0) && (nk + 2 >= 0)) {
-                if ((m[ni + 1][nk + 2] == 0)) {
+            if ((ni + 1 < i) && (nj + 2 < j) && (ni + 1 >= 0) && (nj + 2 >= 0)) {
+                if ((m[ni + 1][nj + 2] == 0)) {
                     numOfHits++;
                 }
             }
-            if ((ni + 1 < i) && (nk - 2 < k) && (ni + 1 >= 0) && (nk - 2 >= 0)) {
-                if ((m[ni + 1][nk - 2] == 0)) {
+            if ((ni + 1 < i) && (nj - 2 < j) && (ni + 1 >= 0) && (nj - 2 >= 0)) {
+                if ((m[ni + 1][nj - 2] == 0)) {
                     numOfHits++;
                 }
             }
-            if ((ni - 1 < i) && (nk - 2 < k) && (ni - 1 >= 0) && (nk - 2 >= 0)) {
-                if ((m[ni - 1][nk - 2] == 0)) {
+            if ((ni - 1 < i) && (nj - 2 < j) && (ni - 1 >= 0) && (nj - 2 >= 0)) {
+                if ((m[ni - 1][nj - 2] == 0)) {
                     numOfHits++;
                 }
             }
-            if ((ni - 1 < i) && (nk + 2 < k) && (ni - 1 >= 0) && (nk + 2 >= 0)) {
-                if ((m[ni - 1][nk + 2] == 0)) {
+            if ((ni - 1 < i) && (nj + 2 < j) && (ni - 1 >= 0) && (nj + 2 >= 0)) {
+                if ((m[ni - 1][nj + 2] == 0)) {
                     numOfHits++;
                 }
                             
