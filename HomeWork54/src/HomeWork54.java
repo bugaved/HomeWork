39236786c2cee62d1f1319f168e51f514f185969
bugaved <bugaved@gmail.com>
2015-11-06 20:29:40 +0300

    commit10

--- HomeWork54/src/HomeWork54.java
+++ HomeWork54/src/HomeWork54.java
@@ -9,37 +9,40 @@ public class HomeWork54 {
         int nk = 0;
         int g = 1;
         int m[][] = new int[i][k];
-        while (x < (i * k)+1) {
+        int number = 0;
+        while (x < (i * k) + 2) {
+            number++;
+            m[ni][nk] = number;
 
-            if ((ni < (i - g)) && (nk == (g - 1))) {
-                System.out.println(ni + " " + nk);
-                x++;
-                ni++;
-            }
-            if ((ni == (i - g)) && (nk < (k - g))) {
-                System.out.println(ni + " " + nk);
-                x++;
-                nk++;
-            }
-            if (nk == (k - g) && (ni >= g)) {
-                System.out.println(ni + " " + nk);
-                x++;
-                ni--;
-            }
             if ((ni == (g - 1)) && (nk >= g)) {
-                System.out.println(ni + " " + nk);
                 x++;
                 nk--;
             }
+            if (nk == (k - g) && (ni >= g)) {
+                x++;
+                ni--;
+            }
+            if ((ni == (i - g)) && (nk < (k - g))) {
+                x++;
+                nk++;
+            }
+            if ((ni <= (i - g)) && (nk == (g - 1))) {
+                x++;
+                ni++;
+            }
             if ((ni == (g - 1)) && (nk == g)) {
                 g++;
-                  x++;
-               
-
+                x++;
             }
 
         }
-          System.out.println(ni + " " + nk);
-        
+        for (nk = 0; nk < k; nk++) {
+            for (ni = 0; ni < i; ni++) {
+                String f = String.format("%02d", m[ni][nk]);
+                System.out.print(f + " ");
+            }
+            System.out.println("");
+        }
+
     }
 }
